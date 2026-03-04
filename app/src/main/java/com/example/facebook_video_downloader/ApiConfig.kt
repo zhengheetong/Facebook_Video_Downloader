73cde5b130a9c5a5d8fbd6084c37f2604a191749
zhengheetong <84259856+zhengheetong@users.noreply.github.com>
2026-03-04 21:29:33 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/facebook_video_downloader/ApiConfig.kt
@@ -0,0 +1,6 @@
+package com.example.facebook_video_downloader
+
+object ApiConfig {
+        const val RAPID_API_KEY = "YOUR_API_KEY_HERE" 
+        const val RAPID_API_HOST = "free-facebook-downloader.p.rapidapi.com" 
+}
