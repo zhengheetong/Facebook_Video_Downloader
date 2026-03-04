c67559a7a45f3325761e60a5cc1a994f722108db
zhengheetong <84259856+zhengheetong@users.noreply.github.com>
2026-03-04 21:26:27 +0800

    Delete app/src/main/java/com/example/facebook_video_downloader/ApiConfig.kt

--- app/src/main/java/com/example/facebook_video_downloader/ApiConfig.kt
+++
@@ -1,6 +0,0 @@
-package com.example.facebook_video_downloader
-
-object ApiConfig {
-    const val RAPID_API_KEY = "Insert Your Own API Key"
-    const val RAPID_API_HOST = "free-facebook-downloader.p.rapidapi.com"
-}
